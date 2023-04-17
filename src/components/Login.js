d978301d477b412c434ff71d297541dca79c8275
Yogendra Rajpoot <90867615+YogendraRajpoot@users.noreply.github.com>
2023-04-17 12:47:31 +0530

    Update Login.js

--- src/components/Login.js
+++ src/components/Login.js
@@ -3,6 +3,7 @@ import { useNavigate } from "react-router-dom";
 import Notes1 from "../assets/notes1.jpg";
 import Notes2 from "../assets/notes2.jpg";
 import Notes3 from "../assets/notes3.avif";
+import Notes4 from "../assets/notes4.jpg";
 
 const Login = (props) => {
   const HEIGHT = window.innerHeight;
@@ -13,16 +14,19 @@ const Login = (props) => {
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    const response = await fetch("https://notesbackend-qr9l.onrender.com/api/auth/login", {
-      method: "POST",
-      headers: {
-        "Content-Type": "application/json",
-      },
-      body: JSON.stringify({
-        email: credentials.email,
-        password: credentials.password,
-      }),
-    });
+    const response = await fetch(
+      "https://notesbackend-qr9l.onrender.com/api/auth/login",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify({
+          email: credentials.email,
+          password: credentials.password,
+        }),
+      }
+    );
     const json = await response.json();
     console.log(json);
     if (json.success) {
@@ -41,19 +45,35 @@ const Login = (props) => {
 
   return (
     <div
-      className=" my-5"
+      className=" py-5"
       style={{
-        height: `${HEIGHT - 0.2 * HEIGHT}px`,
-        width: "95%",
+        height: `${HEIGHT - 0.075 * HEIGHT}px`,
+        width: "100%",
         marginLeft: "auto",
         marginRight: "auto",
         display: "flex",
         flexDirection: "row",
         justifyContent: "space-evenly",
+        backgroundColor: "black",
+        backgroundImage: `url(${Notes3}) `,
+        backgroundSize: "cover",
+        backgroundRepeat: "no-repeat",
+        backgroundPosition: "center",
+        alignItems: "center",
       }}
     >
-      <div className="w-50 ">
-        <img
+      <div
+        style={{
+          display: "flex",
+          flexDirection: "column",
+          alignItems: "center",
+          justifyContent: "center",
+          width: "50%",
+          gap: "30px",
+          fontWeight: "800",
+        }}
+      >
+        {/* <img
           src={Notes1}
           style={{
             maxWidth: "100%",
@@ -61,19 +81,45 @@ const Login = (props) => {
             // objectFit: "cover",
             borderRadius: "10px",
           }}
-        />
+        /> */}
+        <h2
+          style={{
+            textAlign: "center",
+            fontWeight: "800",
+            textShadow: "2px 2px 8px grey",
+          }}
+        >
+          Capture, Organize, and Secure Your Thoughts
+        </h2>
+        <h2
+          style={{
+            textAlign: "center",
+            fontWeight: "800",
+            textShadow: "2px 2px 8px grey",
+          }}
+        >
+          Keep Your Ideas Safe
+        </h2>
+        <h2
+          style={{
+            textAlign: "center",
+            fontWeight: "800",
+            textShadow: "2px 2px 8px grey",
+          }}
+        >
+          Unlock Your Creativity
+        </h2>
       </div>
       <form
         onSubmit={handleSubmit}
         style={{
-          height: `${HEIGHT - 0.5 * HEIGHT}`,
-          minWidth: "25%",
-          // minHeight: "30vw",
-          // minWidth: "35vw",
+          minWidth: "20%",
           borderRadius: "10px",
           boxShadow: "11px 20px 15px grey",
-          padding: "2%",
-          backgroundColor: "#c5ece9",
+          padding: "4%",
+          border: "0.01px solid",
+          // backgroundColor: "#c5ece9",
+          backgroundColor: "#f0967e",
         }}
       >
         <h3 className="text-center">User Login</h3>
